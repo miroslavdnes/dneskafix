11c97245f78d4e92ab6a0efcef4093f2aa92a6ef
miroslavdnes <striro13@gmail.com>
2025-04-05 14:29:54 +0200

    Add files via upload

---
+++ components/Header.js
@@ -0,0 +1,1 @@
+// Tu môže byť komponent hlavičky
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>DNESKA</h1>
+      <p>Ahoj, toto je tvoja prvá live appka 💥</p>
+    </div>
+  );
+}
